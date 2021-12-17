650e55149fd45c0efb51966507fd7772619750b2
aziker13 <87049486+aziker13@users.noreply.github.com>
2021-12-16 23:09:20 -0800

    Add files via upload

---
+++ Data_Engineering/Final_Code/SeaCoffeeRecModel_streamlit.py
@@ -0,0 +1,35 @@
+
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
+from sklearn.metrics import pairwise_distances
+from joblib import load
+import pandas as pd
+import streamlit as st
+
+st.write(
+ '''
+ # What do you love in a coffee shop?
+ '''
+ )
+
+cv = load('SeaCoffeeRecModel_cv.joblib') 
+nmf_model = load('SeaCoffeeRecModel_nmf.joblib')
+df = pd.read_csv('SeaCoffeeRecModel_df.csv', index_col='name')
+
+def get_coffee_recs(string_lst,n_recs=1, df=df,vect=cv,model=nmf_model):
+    vt = cv.transform(string_lst)
+    tt = model.transform(vt)
+    top_n = pairwise_distances(tt,df).argsort().tolist()[0][:n_recs]
+    recs = []
+    for i in top_n:
+        recs.append((df.iloc[i].name))
+    return recs[0]
+
+user_input = st.text_input("Keyword", value='latte')
+shop_name = get_coffee_recs([user_input])
+st.write(
+f'''
+ ## In Seattle I recommend: {shop_name}
+'''
+)
+
